84285803f9ab4af26b44e9815076b02f1b790197
sadikshaik <jafarsadikshaik.48@gmail.com>
2026-01-28 16:17:27 +0530

    DataTypes.java

---
+++ sdk/src/main/java/org/example/DataTypes.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int x=10;
+        float fl=0.3f;
+        byte by=48;
+        short sh=76;
+        long lg=996384682;
+        double db=827.81765727;
+        char ch='c';
+        System.out.println("int:" + x + ", float:" + fl + ", short:" + sh + ", long:" + lg + ", double:" +db+", char:"+ch);
+    }
+}
