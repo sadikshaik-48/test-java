76df70cf5c5fad50a6efff28d4dc9001e0aad51a
sadikshaik <jafarsadikshaik.48@gmail.com>
2026-01-29 21:39:58 +0530

    adding new files

---
+++ sdk/src/main/java/Names.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Names {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        sc.nextLine();
+
+        if (n < 1 || n > 100) return;
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            String names = sc.nextLine();
+
+            if (names.equalsIgnoreCase("mike")) {
+                found = true;
+            }
+        }
+        if (found) {
+            System.out.println("mike");
+        }
+        else {
+            System.out.println("name not found");
+        }
+    }
+
+}
+
